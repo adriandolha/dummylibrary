3fae5fe47a20325ba47ddc8c6a8e951b89958cfa
Adrian Dolha <adriandolha@yahoo.com>
2014-06-30 21:13:12 +0300

    fix search pagination

--- src/main/java/com/disertatie/mylibrary/web/BookSearchController.java
+++ src/main/java/com/disertatie/mylibrary/web/BookSearchController.java
@@ -10,10 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriUtils;
 import org.springframework.web.util.WebUtils;
 
@@ -28,19 +25,8 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/books/search")
+@SessionAttributes("filter")
 public class BookSearchController {
-    public enum SearchType{TITLE("title"), AUTHOR("author"), CATEGORY("category");
-        String title;
-        SearchType(String title) {
-            this.title = title;
-        }
-
-        @Override
-        public String toString() {
-            return title;
-        }
-    }
-    public static final String LIKE_EXPRESSION = "%%%s%%";
     @Autowired
     BookService bookService;
     @Autowired
@@ -49,32 +35,17 @@ public class BookSearchController {
     public EntityManager entityManager;
     @RequestMapping(method = RequestMethod.POST, produces = "text/html")
     public String search(BookSearch bookSearch, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
-        SearchType searchType = SearchType.valueOf(bookSearch.getSearchType().toUpperCase());
-        int sizeNo = 10;
-        Integer page = 1;
-        JPAQuery query = new JPAQuery(entityManager);
-        QBook books = QBook.book;
-        List<Book> resultBooks = null;
-        switch (searchType){
-            case TITLE:
-                resultBooks = query.from(books).where(
-                        books.title.like(String.format(LIKE_EXPRESSION, bookSearch.getValue())))
-                        .list(books);
-                break;
-            case AUTHOR:
-                resultBooks = bookRepository.findByAuthorLike(bookSearch.getValue());
-                break;
-            case CATEGORY:
-                resultBooks = bookRepository.findByCategoryLike(bookSearch.getValue());
-                break;
-        }
-        uiModel.addAttribute("books", resultBooks);
+        uiModel.addAttribute("filter", bookSearch);
+        List<Book> resultBooks = bookService.getBooks(bookSearch);
+        int sizeNo = 5;
         float nrOfPages = (float) resultBooks.size() / sizeNo;
+        uiModel.addAttribute("books", resultBooks.subList(0, 5));
         uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+        uiModel.addAttribute("size", sizeNo);
         return "books/searchList";
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "form", method = RequestMethod.GET)
     public String searchForm(Model uiModel) {
         uiModel.addAttribute(new BookSearch());
         return "books/searchForm";
@@ -82,15 +53,23 @@ public class BookSearchController {
 
     @RequestMapping(produces = "text/html")
     public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
+        BookSearch bookSearch = (BookSearch) uiModel.asMap().get("filter");
         if (page != null || size != null) {
-            int sizeNo = size == null ? 10 : size.intValue();
+            int sizeNo = size == null ? 5 : size.intValue();
             final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
-            uiModel.addAttribute("books", bookService.findBookEntries(firstResult, sizeNo, sortFieldName, sortOrder));
-            float nrOfPages = (float) bookRepository.count() / sizeNo;
+            List<Book> books = null;
+            if (bookSearch == null){
+                books = bookService.findBookEntries(firstResult, sizeNo);
+                size = books.size();
+            } else{
+                BookService.Page bookEntries = bookService.findBookEntries(bookSearch, firstResult, sizeNo);
+                books = bookEntries.getBooks();
+                size = bookEntries.getSize();
+            }
+            uiModel.addAttribute("books", books);
+            float nrOfPages = (float) size / sizeNo;
             uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
-        } else {
-            Sort sort = sortOrder != null && sortFieldName!= null? new Sort(Sort.Direction.fromString(sortOrder), sortFieldName):null;
-            uiModel.addAttribute("books", bookRepository.findAll(sort));
+            uiModel.addAttribute("size", sizeNo);
         }
         return "books/searchList";
     }
--- src/main/java/com/disertatie/mylibrary/web/BookService.java
+++ src/main/java/com/disertatie/mylibrary/web/BookService.java
@@ -2,8 +2,12 @@ package com.disertatie.mylibrary.web;
 
 import com.disertatie.mylibrary.domain.Book;
 import com.disertatie.mylibrary.domain.Book;
+import com.disertatie.mylibrary.domain.QBook;
 import com.disertatie.mylibrary.repository.BookRepository;
+import com.mysema.query.jpa.impl.JPAQuery;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
@@ -12,6 +16,19 @@ import java.util.List;
 
 @Service
 public class BookService {
+    public static final String LIKE_EXPRESSION = "%%%s%%";
+    public enum SearchType{TITLE("title"), AUTHOR("author"), CATEGORY("category");
+        String title;
+        SearchType(String title) {
+            this.title = title;
+        }
+
+        @Override
+        public String toString() {
+            return title;
+        }
+    }
+
     public static final List<String> fieldNames4OrderClauseFilter = java.util.Arrays.asList("");
     @Autowired
     BookRepository bookRepository;
@@ -40,4 +57,56 @@ public class BookService {
         return entityManager.createQuery(jpaQuery, Book.class).setFirstResult(firstResult).setMaxResults(maxResults).getResultList();
     }
 
+    public List<Book> getBooks(BookSearch bookSearch) {
+        SearchType searchType = SearchType.valueOf(bookSearch.getSearchType().toUpperCase());
+        JPAQuery query = new JPAQuery(entityManager);
+        QBook books = QBook.book;
+        List<Book> resultBooks = null;
+        switch (searchType){
+            case TITLE:
+                resultBooks = query.from(books).where(
+                        books.title.like(String.format(LIKE_EXPRESSION, bookSearch.getValue())))
+                        .list(books);
+                break;
+            case AUTHOR:
+                resultBooks = bookRepository.findByAuthorLike(bookSearch.getValue());
+                break;
+            case CATEGORY:
+                resultBooks = bookRepository.findByCategoryLike(bookSearch.getValue());
+                break;
+        }
+        return resultBooks;
+    }
+
+    public Page findBookEntries(BookSearch bookSearch, int firstResult, int maxResults) {
+        List<Book> books = getBooks(bookSearch);
+        int to = firstResult + maxResults;
+        return new Page(books.subList(firstResult, to < books.size() ? to : books.size()), books.size());
+    }
+
+    public static class Page{
+        List<Book> books;
+        int size;
+
+        public Page(List<Book> books, int size) {
+            this.books = books;
+            this.size = size;
+        }
+
+        public List<Book> getBooks() {
+            return books;
+        }
+
+        public void setBooks(List<Book> books) {
+            this.books = books;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public void setSize(int size) {
+            this.size = size;
+        }
+    }
 }
